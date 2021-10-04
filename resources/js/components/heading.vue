531d6b3829814166e76a88efa337b55c3d370aa5
hosimiya7 <nerokarurin@gmail.com>
2021-10-04 16:49:47 +0900

    icon追加

--- resources/js/components/heading.vue
+++ resources/js/components/heading.vue
@@ -1,18 +1,22 @@
 <template>
-    <div class="heading-wrapper">
-        <ul>
-            <li>
-                works
-            </li>
-            <li>
-                service
-            </li>
-            <li>
-                about
-            </li>
-            <li>
-                skill
-            </li>
-        </ul>
+  <div class="heading-wrapper">
+    <ul>
+      <li>works</li>
+      <li>service</li>
+      <li>about</li>
+      <li>skill</li>
+    </ul>
+    <div class="icon">
+      <div class="twitter-icon">
+        <a href="https://twitter.com/hosimiyaprog">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </div>
+      <div class="github-icon">
+        <a href="https://github.com/hosimiya7">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
     </div>
+  </div>
 </template>
