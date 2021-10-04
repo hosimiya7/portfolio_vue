4d835d2fa01088a92380af6f040d4d691133be43
hosimiya7 <nerokarurin@gmail.com>
2021-10-04 16:48:27 +0900

    img追加

--- resources/js/components/work.vue
+++ resources/js/components/work.vue
@@ -2,6 +2,14 @@
     <div class="work-wrapper">
         <div class="flex">
             <heading></heading>
+            <div class="flex work-list">
+                <a href="https://hosimiya7.github.io/Japanese_sweets/" target="_blank">
+                    <img src="/img/work-1.png" alt="">
+                </a>
+                <a href="https://hosimiya7.github.io/hoshikake_site2/" target="_blank">
+                    <img src="/img/work-2.png" alt="">
+                </a>
+            </div>
         </div>
     </div>
 </template>
