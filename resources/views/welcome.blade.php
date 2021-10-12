8971faaa8a294b293dcf57212297a8e854737a9b
hosimiya7 <nerokarurin@gmail.com>
2021-10-12 10:34:51 +0900

    font調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <meta name="description" content="ホームページ制作をしている星宮のポートフォリオです。「魅力を最大限伝える」ことを第一に、 お客様の目的に沿った「魅せ方」を提案させていただきます。お気軽にご相談ください ">
+    <meta name="description"
+        content="ホームページ制作をしている星宮のポートフォリオです。「魅力を最大限伝える」ことを第一に、 お客様の目的に沿った「魅せ方」を提案させていただきます。お気軽にご相談ください ">
     <meta property="og:site_name" content="hosimiya's portfolio">
     <meta name="twitter:card" content="summary" />
     <meta property="og:url" content="http://hosimiya.site/" />
@@ -23,6 +24,9 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/ang3ldx.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
     {{-- icon --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
 
